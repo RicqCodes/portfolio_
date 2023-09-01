4a49081a8f5bca12228be6d170cdcacad2ee9e27
ricqCodes <princenwakanma1996@gmail.com>
2023-09-01 15:35:24 +0100

    Feat:Added dark theme to use user default settings

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
 
 // set up detection of system dark mode
-const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
 export const useDarkMode = () => {
   // perform an initial detection of system theme
-  const [theme, setTheme] = useState(darkModeQuery.matches ? "dark" : "light");
+  const [theme, setTheme] = useState("");
 
   const setMode = (mode: string) => {
     window.localStorage.setItem("theme", mode);
@@ -17,14 +16,18 @@ export const useDarkMode = () => {
   };
 
   useEffect(() => {
+    const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    setTheme(darkModeQuery.matches ? "dark" : "light");
+
     darkModeQuery.addEventListener("change", (event) => {
       setMode(event.matches ? "dark" : "light");
     });
-  });
+  }, []);
 
   useEffect(() => {
+    console.log("from the last effect", theme);
     const localTheme = window.localStorage.getItem("theme") || theme;
     localTheme && setTheme(localTheme);
-  }, []);
+  }, [theme]);
   return { theme, themeToggler };
 };
