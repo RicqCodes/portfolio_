3d5f63ad6433f1b2f2e9e9f9d4d67f818f754a43
ricqCodes <princenwakanma1996@gmail.com>
2023-09-02 12:42:28 +0100

    Added analytic feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import type { Metadata } from "next";
 import { Inter, Poppins } from "next/font/google";
 
@@ -43,6 +44,7 @@ export default function RootLayout({
         <StyledComponentsRegistry>
           <Layout>{children}</Layout>
         </StyledComponentsRegistry>
+        <Analytics />
       </body>
     </html>
   );
